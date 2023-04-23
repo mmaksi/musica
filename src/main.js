3aa2049448feb63d66c9ca9f079a64742b904f85
mmaksi <mmaksi.dev@gmail.com>
2023-04-23 15:17:04 +0200

    Adding global directive for icons

--- src/main.js
+++ src/main.js
@@ -10,12 +10,18 @@ import './assets/base.css'
 import './includes/firebase'
 import VeeValidatePlugin from './includes/validation'
 import { auth } from './includes/firebase'
+import Icon from './directives/icon'
 
 let authFlag = true
 auth.onAuthStateChanged(() => {
   // To mount the Vue instance only once
   if (authFlag) {
     authFlag = false
-    createApp(App).use(router).use(createPinia()).use(VeeValidatePlugin).mount('#app')
+    createApp(App)
+      .use(router)
+      .use(createPinia())
+      .use(VeeValidatePlugin)
+      .directive('icon', Icon)
+      .mount('#app')
   }
 })
